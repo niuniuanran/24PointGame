e41cacccea43008616ff69199b2471405771f718
anran <aniu423@aucklanduni.ac.nz>
2019-12-06 23:12:16 +1300

    24.py helps check whether four numbers will work.  -- by Yihao

---
+++ 24.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+while True:
+    nums = []
+    data = input("Please input 4 whole numbers:\n")
+    data = data.split()
+    if len(data) != 4:
+        print("Invalid input!\n")
+        continue
+    try:
+        for n in data:
+            nums.append(int(n))
+        break
+    except:
+        print("Invalid input!\n")
+nums = list(map(str, nums))
+ans = []
+ops = ['+', '-', '*', '/']
+for i in range(4):
+    for j in range(4):
+        for k in range(4):
+            ans.append(''.join(['((', nums[0], ops[i], nums[1], ')', ops[j], nums[2], ')', ops[k], nums[3]]))
+            ans.append(''.join(['(' , nums[0], ops[i], '(', nums[1], ops[j], nums[2], '))', ops[k], nums[3]]))
+            ans.append(''.join(['(' , nums[0], ops[i], nums[1], ')', ops[j], '(', nums[2], ops[k], nums[3], ')']))
+            ans.append(''.join([nums[0], ops[i], '((', nums[1], ops[j], nums[2], ')', ops[k], nums[3], ')']))
+            ans.append(''.join([nums[0], ops[i], '(' + nums[1], ops[j],'(', nums[2], ops[k], nums[3], '))']))
+res = []
+for i in range(len(ans)):
+    try:
+        r = eval(ans[i])
+        res.append(r)
+    except:
+        res.append(0)
+count = 0
+for i in range(len(res)):
+    if res[i] == 24:
+        count += 1
+        print("solution {}: {}\n".format(count, ans[i]))
+if count == 0:
+    print("No possible solution\n")
